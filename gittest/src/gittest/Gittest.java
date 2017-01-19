9aa1aecc2714bda21fa32425e906af6423c15750
손진무 <youcanja1@gmail.com>
2017-01-19 11:03:50 +0900

    github 연동 test

---
+++ gittest/src/gittest/Gittest.java
@@ -0,0 +1,13 @@
+package gittest;
+
+import java.util.Scanner;
+
+public class Gittest {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		Scanner fScanner = new Scanner(System.in);
+		
+	}
+
+}
